b7ba6bb049ad2af2c2dbf172d0b2bd01939b82bb
Caio Messias <caio.messias@gerencianet.com.br>
2021-12-03 02:48:24 +0000

    Criado rotas para cadastro de Specifications e alterando services para useCases

---
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -0,0 +1,26 @@
+import { ISpecificationsRepository } from '../repositories/ISpecificationsRepository';
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateSpecificationService {
+  constructor(private specificationsRepository: ISpecificationsRepository) {}
+
+  execute({ name, description }: IRequest): void {
+    const specificationAlreadExists =
+      this.specificationsRepository.findByName(name);
+
+    if (specificationAlreadExists) {
+      throw new Error('Specification alread exists!');
+    }
+
+    this.specificationsRepository.create({
+      name,
+      description,
+    });
+  }
+}
+
+export { CreateSpecificationService };
