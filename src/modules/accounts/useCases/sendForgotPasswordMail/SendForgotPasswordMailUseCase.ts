bb727cafbe2d93342c3d203e44d60ab21673a6d2
Caio Messias <caio.messias@gerencianet.com.br>
2022-03-22 10:56:53 +0000

    Continua recuperação de senha

--- src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -1,9 +1,12 @@
 import { inject, injectable } from "tsyringe";
 import { v4 as uuidV4 } from 'uuid';
+import { resolve } from 'path';
+
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
 import { IUsersTokensRepository } from '@modules/accounts/repositories/IUsersTokensRepository';
 import { AppError } from "@shared/errors/AppError";
 import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
+import { IMailProvider } from "@shared/container/providers/MailProvider/IMailProvider";
 
 @injectable()
 class SendForgotPasswordMailUseCase {
@@ -16,9 +19,14 @@ class SendForgotPasswordMailUseCase {
 
     @inject('DayjsDateProvider')
     private dateProvider: IDateProvider,
+    
+    @inject('EtherealMailProvider')
+    private mailProvider: IMailProvider,
   ) {}
   async execute(email: string) {
     const user = await this.usersRepository.findByEmail(email);
+
+    const templatePath = resolve(__dirname, '..', '..', 'views', 'emails', 'forgotPassword.hbs')
 
     if(!user) {
       throw new AppError('User does not exists!');
@@ -33,6 +41,18 @@ class SendForgotPasswordMailUseCase {
       user_id: user.id,
       expires_date
     });
+
+    const variables = {
+      name: user.name,
+      link: `${process.env.FORGOT_MAIL_URL}${token}`
+    };
+
+    await this.mailProvider.sendMail(
+      email, 
+      'Recuperação de senha', 
+      variables,
+      templatePath
+    );
   }
 }
 
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -4,6 +4,7 @@ import { authenticateRoutes } from './authenticate.routes';
 import { carsRoutes } from './cars.routes';
 import { categoriesRoutes } from './categories.routes';
 import { rentalRoutes } from './rental.routes';
+import { passwordRoutes } from './password.routes';
 import { specificationsRoutes } from './specifications.routes';
 import { usersRoutes } from './user.routes';
 
@@ -14,6 +15,7 @@ router.use('/specifications', specificationsRoutes);
 router.use('/users', usersRoutes);
 router.use('/cars', carsRoutes);
 router.use('/rentals', rentalRoutes);
+router.use('/password', passwordRoutes);
 router.use(authenticateRoutes);
 
 export { router };
