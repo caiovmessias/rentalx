0be5b697dc36e8847e2319fd3d1d56a263100305
Caio Messias <caio.messias@gerencianet.com.br>
2022-03-23 11:10:04 +0000

    Refatoração de testes

--- src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -11,22 +11,48 @@ class EtherealMailProvider implements IMailProvider {
   private client: Transporter;
 
   constructor() {
-    nodemailer.createTestAccount().then(account => {
-      const transporter = nodemailer.createTransport({
+    this.createClient();
+  }
+
+  private async createClient() {
+    try {
+      const account = await nodemailer.createTestAccount();
+  
+      this.client = nodemailer.createTransport({
         host: account.smtp.host,
         port: account.smtp.port,
         secure: account.smtp.secure,
         auth: {
           user: account.user,
           pass: account.pass,
-        }
-      })
-
-      this.client = transporter;
-    }).catch((err) => console.error(err));
+        },
+      });
+    } catch (err) {
+      console.error(`EtherealMailProvider - Error:\n${err}`);
+    }
   }
 
+  // constructor() {
+  //   nodemailer.createTestAccount().then(account => {
+  //     const transporter = nodemailer.createTransport({
+  //       host: account.smtp.host,
+  //       port: account.smtp.port,
+  //       secure: account.smtp.secure,
+  //       auth: {
+  //         user: account.user,
+  //         pass: account.pass,
+  //       }
+  //     })
+
+  //     this.client = transporter;
+  //   }).catch((err) => console.error(err));
+  // }
+
   async sendMail(to: string, subject: string, variables: any, path: string): Promise<void> {
+    if (!this.client) {
+      await this.createClient();
+    }
+
     const templateFileContent = fs.readFileSync(path).toString('utf-8');
 
     const templateParse = handlebars.compile(templateFileContent);
