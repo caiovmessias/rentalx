c98c01f30ecd12e9fafdb4cb87f6942d8b41bcfb
Caio Messias <caio.messias@gerencianet.com.br>
2022-08-19 13:35:11 +0000

    altera logica do token

--- src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
+++ src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
@@ -1,13 +1,19 @@
-import auth from '@config/auth'
-import { IUsersTokensRepository } from '@modules/accounts/repositories/IUsersTokensRepository'
+import { verify, sign } from 'jsonwebtoken';
+import { inject, injectable } from 'tsyringe';
+
+import auth from '@config/auth';
+import { IUsersTokensRepository } from '@modules/accounts/repositories/IUsersTokensRepository';
 import { IDateProvider } from '@shared/container/providers/DateProvider/IDateProvider';
 import { AppError } from '@shared/errors/AppError';
-import { verify, sign } from 'jsonwebtoken'
-import { inject, injectable } from 'tsyringe'
 
 interface IPayload {
   sub: string;
   email: string;
+}
+
+interface ITokenResponse {
+  token: string;
+  refresh_token: string;
 }
 
 @injectable()
@@ -20,14 +26,18 @@ class RefreshTokenUseCase {
     private dateProvider: IDateProvider,
   ) {}
 
-  async execute(token: string): Promise<string> {
+  async execute(token: string): Promise<ITokenResponse> {
     const { email, sub } = verify(token, auth.secret_refresh_token) as IPayload;
-    
+
     const user_id = sub;
 
-    const userToken = await this.usersTokensRepository.findByUserIdAndRefreshToken(user_id, token);
+    const userToken =
+      await this.usersTokensRepository.findByUserIdAndRefreshToken(
+        user_id,
+        token,
+      );
 
-    if(!userToken) {
+    if (!userToken) {
       throw new AppError('Refresh Token does not exists!');
     }
 
@@ -35,20 +45,29 @@ class RefreshTokenUseCase {
 
     const refresh_token = sign({ email }, auth.secret_refresh_token, {
       subject: sub,
-      expiresIn: auth.expires_in_refresh_token
+      expiresIn: auth.expires_in_refresh_token,
     });
 
-    const expires_date = this.dateProvider.addDays(auth.expires_refresh_token_days);
+    const expires_date = this.dateProvider.addDays(
+      auth.expires_refresh_token_days,
+    );
 
     await this.usersTokensRepository.create({
       expires_date,
       refresh_token,
-      user_id
-    })
+      user_id,
+    });
 
-    return refresh_token;
+    const newToken = sign({}, auth.secret_token, {
+      subject: user_id,
+      expiresIn: auth.expires_in_token,
+    });
 
+    return {
+      refresh_token,
+      token: newToken,
+    };
   }
 }
 
-export { RefreshTokenUseCase }
+export { RefreshTokenUseCase };
--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -1,10 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
 
-import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
-import { AppError } from '@shared/errors/AppError';
-import { UsersTokensRepository } from '@modules/accounts/infra/typeorm/repositories/UsersTokensRepository';
 import auth from '@config/auth';
+import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
+import { UsersTokensRepository } from '@modules/accounts/infra/typeorm/repositories/UsersTokensRepository';
+import { AppError } from '@shared/errors/AppError';
 
 interface IPayload {
   sub: string;
@@ -17,8 +17,6 @@ export async function ensureAuthenticate(
 ) {
   const authHeader = request.headers.authorization;
 
-  const userTokensRepository = new UsersTokensRepository();
-
   if (!authHeader) {
     throw new AppError('Token missing', 401);
   }
@@ -26,16 +24,7 @@ export async function ensureAuthenticate(
   const [, token] = authHeader.split(' ');
 
   try {
-    const { sub: user_id } = verify(
-      token,
-      auth.secret_refresh_token
-    ) as IPayload;
-
-    const user = await userTokensRepository.findByUserIdAndRefreshToken(user_id, token);
-
-    if (!user) {
-      throw new AppError('User does not exists', 401);
-    }
+    const { sub: user_id } = verify(token, auth.secret_token) as IPayload;
 
     request.user = {
       id: user_id,
