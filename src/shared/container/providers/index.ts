170e331250631e8c9e20791701ffd30107bdcab3
Caio Messias <caio.messias@gerencianet.com.br>
2022-08-22 15:47:57 +0000

    finaliza configuração para upload no s3

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -2,8 +2,11 @@ import { container } from 'tsyringe';
 
 import { IDateProvider } from './DateProvider/IDateProvider';
 import { DayjsDateProvider } from './DateProvider/implementations/DayjsDateProvider';
-import { IMailProvider } from './/MailProvider/IMailProvider';
+import { IMailProvider } from './MailProvider/IMailProvider';
 import { EtherealMailProvider } from './MailProvider/implementations/EtherealMailProvider';
+import { LocalStorageProvider } from './storageProvider/implementations/LocalStorageProvider';
+import { S3StorageProvider } from './storageProvider/implementations/S3StorageProvider';
+import { IStorageProvider } from './storageProvider/IStorageProvider';
 
 container.registerSingleton<IDateProvider>(
   'DayjsDateProvider',
@@ -13,4 +16,14 @@ container.registerSingleton<IDateProvider>(
 container.registerInstance<IMailProvider>(
   'EtherealMailProvider',
   new EtherealMailProvider(),
+);
+
+const diskStorage = {
+  local: LocalStorageProvider,
+  s3: S3StorageProvider,
+};
+
+container.registerSingleton<IStorageProvider>(
+  'StorageProvider',
+  diskStorage[process.env.disk],
 );
